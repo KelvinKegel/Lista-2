07c6f7bdbfc8fb6177cb38e1305eb544415a32f6
KelvinKegel <51788952+KelvinKegel@users.noreply.github.com>
2019-07-18 12:15:28 -0300

    primeiro upload

---
+++ survivor 3d/Assets/Scripts/Movementnovo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movementnovo : MonoBehaviour
+{
+    float horizontal;
+    Rigidbody body;
+
+    [SerializeField]
+    float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        body.velocity = new Vector3(horizontal * speed, 0, 0);
+    }
+}
