7631b90235a8aebb41292dc971d57c2d73579f28
KelvinKegel <51788952+KelvinKegel@users.noreply.github.com>
2019-07-22 12:17:44 -0300

    enemy

--- survivor 3d/Assets/Scripts/Enemy.cs
+++ survivor 3d/Assets/Scripts/Enemy.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject Player;
+    public float movementSpeed = 4;
     void Update()
     {
-        
+        transform.LookAt(Player.transform);
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
     }
 }
